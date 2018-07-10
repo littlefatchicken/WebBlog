32b8b8ce15b44c06b7833f237cbe7329abddae38
wing57 <1326104685@qq.com>
2018-07-10 17:36:15 +0800

    123

--- js/app.js
+++ js/app.js
@@ -26,6 +26,24 @@ $(function () {
         $("body").removeClass("Body_noscroll");
     })
     footer_anrun();
+    //about
+    $('.about_scrolltext01').liMarquee(
+        {
+            hoverstop: false,
+            scrollamount: 20
+        }
+    );
+    $('.about_scrolltext02').liMarquee({
+        direction: 'right',
+        hoverstop: false,
+        scrollamount: 20
+    });
+    $('.about_scrolltext03').liMarquee(
+        {
+            hoverstop: false,
+            scrollamount: 20
+        }
+    );
 
 })
 function footer_anrun() {
@@ -44,7 +62,7 @@ function footer_anrun() {
         $('.footer_inner_0102 a').removeClass('footer_inner_0102an');
     }).mouseleave(function () {
         count = $(this).index() + 1;
-        footer_antime=setInterval(footer_an, 1000);
+        footer_antime = setInterval(footer_an, 1000);
     });
     // $('.footer_inner_0102 a').hover(function(){
     //     clearInterval(footer_antime);
